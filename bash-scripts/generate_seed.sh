43b5c9f3c903dae45280b4fad64831c1a9c04af8
microengineer <microengineer18@gmail.com>
2020-02-13 13:04:33 +0000

    changed scripts from using picocom to using serialport nodejs

--- bash-scripts/generate_seed.sh
+++ bash-scripts/generate_seed.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo '{"command":"generateRandomSeed","key": 0}' | sudo picocom --baud 115200 --echo --imap crcrlf --exit-after 9000 /dev/ttyS0
+node ../node-scripts/serial.js '{"command":"generateRandomSeed","key": 0}'
+#echo '{"command":"generateRandomSeed","key": 0}' | sudo picocom --baud 115200 --echo --imap crcrlf --exit-after 9000 /dev/ttyS0
--- bash-scripts/init_secure_element.sh
+++ bash-scripts/init_secure_element.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo '{"command": "initSecureElement","key": "3780e63d4968ade5d822c013fcc323845d1b569fe705b60006feec145a0db1e3"}' | sudo picocom --baud 115200 --echo --imap crcrlf --exit-after 1000 /dev/ttyS0
+node ../node-scripts/serial.js '{"command": "initSecureElement","key": "3780e63d4968ade5d822c013fcc323845d1b569fe705b60006feec145a0db1e3"}'
+#echo '{"command": "initSecureElement","key": "3780e63d4968ade5d822c013fcc323845d1b569fe705b60006feec145a0db1e3"}' | sudo picocom --baud 115200 --echo --imap crcrlf --exit-after 1000 /dev/ttyS0
