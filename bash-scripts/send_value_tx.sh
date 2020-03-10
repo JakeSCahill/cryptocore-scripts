c7cdefe762529d112a02d434c22bf6893592445d
Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
2020-03-10 09:47:12 +0000

    Update send_value_tx.sh

--- bash-scripts/send_value_tx.sh
+++ bash-scripts/send_value_tx.sh
@@ -33,7 +33,7 @@ else
     keyIndex=$(tail -n 1 $indexFile | jq .index)
 fi
 
-# Make sure a directory exists in which to save the transaction trytes
+# Make sure a directory exists in which you can save unfinished or pending transactions
 saved_transaction_directory="/home/pi/cryptocore-scripts/my_transactions"
 
 if [ ! -d $saved_transaction_directory ]; then
